b7c3691ef37e0ee7dfef09331b113662068e5a29
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-10 20:49:23 -0300

    :wrench: chore: Create App structure

---
+++ src/screens/Edit/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { Container } from './styles'
+
+export function Edit() {
+  return (
+    <Container>
+      <Text>Edit</Text>
+    </Container>
+  )
+}
---
+++ src/screens/Feedback/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { Container } from './styles'
+
+export function Feedback() {
+  return (
+    <Container>
+      <Text>Feedback</Text>
+    </Container>
+  )
+}
